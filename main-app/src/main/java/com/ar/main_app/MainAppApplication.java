8d5086a5e2fda3205a0566f62cfa7e41946716f5
A R Danish <helloardanish@gmail.com>
2025-05-12 23:01:06 +0530

    Using User model in main app as a model
    Run the project using command: mvn spring-boot:run from main-app folder or mvn -pl main-app spring-boot:run from root folder

--- main-app/src/main/java/com/ar/main_app/MainAppApplication.java
+++ main-app/src/main/java/com/ar/main_app/MainAppApplication.java
@@ -1,13 +1,22 @@
 package com.ar.main_app;
 
+import com.ar.model.models.User;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class MainAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainAppApplication.class, args);
+		test();
+	}
+
+	public static void test(){
+		User user = new User("ar", "1234");
+		log.info(user);
 	}
 
 }
