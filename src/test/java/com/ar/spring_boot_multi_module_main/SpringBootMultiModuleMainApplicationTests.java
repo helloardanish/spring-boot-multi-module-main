bb2a19987edfb1eb43c80b8200ad2be8efbc8268
A R Danish <helloardanish@gmail.com>
2025-05-12 22:36:33 +0530

    first commit

---
+++ src/test/java/com/ar/spring_boot_multi_module_main/SpringBootMultiModuleMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.spring_boot_multi_module_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiModuleMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
